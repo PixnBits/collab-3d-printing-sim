2a454f3b056575501b72b1727517cad5b6dc4232
Nick Oliver <i@pixnbits.org>
2025-08-24 21:55:19 -0700

    feat: enhance xarm_lite robot model with kinematics and semantic description

--- ws/src/multi_arm_sim/launch/simulation.launch.py
+++ ws/src/multi_arm_sim/launch/simulation.launch.py
@@ -8,10 +8,18 @@ def generate_launch_description():
     pkg_share = get_package_share_directory('multi_arm_sim')
     world_path = os.path.join(pkg_share, 'worlds', 'print_world.sdf')
 
+    urdf_path = os.path.join(pkg_share, 'urdf', 'xarm_lite.urdf')
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    srdf_path = os.path.join(pkg_share, 'config', 'xarm_lite.srdf')
+    with open(srdf_path, 'r') as infp:
+        robot_desc_semantic = infp.read()
+
     return LaunchDescription([
-        # Launch Gazebo with print world
+        # Launch Gazebo server headless
         ExecuteProcess(
-            cmd=['gazebo', '--verbose', world_path, '-s', 'libgazebo_ros_factory.so'],
+            cmd=['gzserver', '--verbose', world_path, '-s', 'libgazebo_ros_factory.so'],
             output='screen'
         ),
         # Launch MoveIt for motion planning
@@ -19,6 +27,7 @@ def generate_launch_description():
             package='moveit_ros_move_group',
             executable='move_group',
             output='screen',
-            parameters=[{'robot_description': os.path.join(pkg_share, 'urdf', 'xarm_lite.urdf')}]
+            parameters=[{'robot_description': robot_desc,
+                         'robot_description_semantic': robot_desc_semantic}]
         ),
     ])
